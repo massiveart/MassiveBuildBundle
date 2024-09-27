396dce03ea2256d6f856f42b156a04e417867cd2
mamazu <14860264+mamazu@users.noreply.github.com>
2024-09-27 12:21:12 +0200

    Replace `simple-phpunit` with `phpunit` (#31)

--- Tests/Build/BuilderContextTest.php
+++ Tests/Build/BuilderContextTest.php
@@ -22,7 +22,7 @@ class BuilderContextTest extends BaseTestCase
     protected $input;
     protected $output;
 
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
         $this->input = $this->prophesize('Symfony\Component\Console\Input\InputInterface');
